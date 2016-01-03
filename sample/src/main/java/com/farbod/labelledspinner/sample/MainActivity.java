d8a3449b5cbf431cba508b5a89e9ffc742947e19
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-01-03 16:46:07 +0000

    Add sample app
    Resolves #7

---
+++ sample/src/main/java/com/farbod/labelledspinner/sample/MainActivity.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 Farbod Salamat-Zadeh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.farbod.labelledspinner.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.farbod.labelledspinner.LabelledSpinner;
+
+public class MainActivity extends AppCompatActivity implements LabelledSpinner.OnItemChosenListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        LabelledSpinner labelledSpinner = (LabelledSpinner) findViewById(R.id.spinner_planets);
+        labelledSpinner.setItemsArray(R.array.planets_array);
+        labelledSpinner.setOnItemChosenListener(this);
+    }
+
+    @Override
+    public void onItemChosen(View labelledSpinner, AdapterView<?> adapterView, View itemView, int position, long id) {
+        String selectedText = adapterView.getItemAtPosition(position).toString();
+        switch (labelledSpinner.getId()) {
+            case R.id.spinner_planets:
+                Toast.makeText(MainActivity.this, "Selected: " + selectedText, Toast.LENGTH_SHORT).show();
+                break;
+            // If you have multiple LabelledSpinners, you can add more cases here
+        }
+    }
+
+    @Override
+    public void onNothingChosen(View labelledSpinner, AdapterView<?> adapterView) {
+        // Do something here
+    }
+}
