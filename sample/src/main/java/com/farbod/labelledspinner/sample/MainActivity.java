4a95b9dcab88c9373078b13cf56784460cab42b5
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-01-16 15:07:28 +0000

    Fix errors where v1.1.4 features do not work

--- sample/src/main/java/com/farbod/labelledspinner/sample/MainActivity.java
+++ sample/src/main/java/com/farbod/labelledspinner/sample/MainActivity.java
@@ -37,8 +37,9 @@ public class MainActivity extends AppCompatActivity implements LabelledSpinner.O
 
         LabelledSpinner labelledSpinner = (LabelledSpinner) findViewById(R.id.spinner_planets);
         labelledSpinner.setItemsArray(R.array.planets_array);
-        labelledSpinner.setDefaultErrorEnabled(true);
-        labelledSpinner.setDefaultErrorText("This is a compulsory field.");  // Displayed when first item remains selected
+        /* The following do not work prior to v1.1.4 (not released yet)! */
+        //labelledSpinner.setDefaultErrorEnabled(true);
+        //labelledSpinner.setDefaultErrorText("This is a compulsory field.");  // Displayed when first item remains selected
         labelledSpinner.setOnItemChosenListener(this);
     }
 
