e73495caae344ddf08f3be88bc7a620f5dfdf1be
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-03-27 22:29:31 +0100

    Ignore error checks for Javadoc checks for bintrayUpload
    Resolves #26.
    (Answer found from: https://github.com/novoda/bintray-release/issues/71#issuecomment-164324255)
    
    Also updates version to 2.0.1

--- library/build/generated/source/buildConfig/debug/com/satsuware/usefulviews/BuildConfig.java
+++ library/build/generated/source/buildConfig/debug/com/satsuware/usefulviews/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.satsuware.usefulviews";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 200;
-  public static final String VERSION_NAME = "2.0.0";
+  public static final int VERSION_CODE = 201;
+  public static final String VERSION_NAME = "2.0.1";
 }
